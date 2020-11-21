9f040530bdcb4af3539980dd17e800abd0ca0642
Chris Snow <chsnow123@gmail.com>
2020-11-21 21:46:30 +0000

    Update entrypoint.sh

--- appconfig/entrypoint.sh
+++ appconfig/entrypoint.sh
@@ -6,7 +6,7 @@ echo *******
 echo $$
 echo ******
 
-if [ "$$" = 1 ]; then
+##if [ "$$" = 1 ]; then
     #
     # Setup envinronment for Kubernetes deployment.
     #
@@ -23,7 +23,7 @@ if [ "$$" = 1 ]; then
         export MAPR_TZ="UTC"
     fi
 
-    if [ -e /mnt/mapr-cluster-secret ]; then
+##    if [ -e /mnt/mapr-cluster-secret ]; then
         MAPR_CONTAINER_USER=${MAPR_CONTAINER_USER:-$MAPR_USER}
         MAPR_CONTAINER_USER=${MAPR_CONTAINER_USER:-$(cat "/mnt/mapr-cluster-secret/MAPR_CONTAINER_USER" 2>/dev/null)}
         MAPR_CONTAINER_USER=${MAPR_CONTAINER_USER:-$(cat "/mnt/mapr-cluster-secret/MAPR_USER" 2>/dev/null)}
@@ -67,7 +67,7 @@ if [ "$$" = 1 ]; then
         mapr_tickefile="/mnt/mapr-cluster-secret/CONTAINER_TICKET"
         MAPR_TICKETFILE_LOCATION=${MAPR_TICKETFILE_LOCATION:-$([ -e "$mapr_tickefile" ] && echo "$mapr_tickefile")}
         [ -n "$MAPR_TICKETFILE_LOCATION" ] && export MAPR_TICKETFILE_LOCATION
-    fi
+##    fi
 
 
     if echo "$ZEPPELIN_PORT" | grep -q "^tcp"; then
@@ -99,7 +99,7 @@ if [ "$$" = 1 ]; then
     # Following piece copied from "container_post_client" function of "mapr-setup.sh"
     exec sudo -E -H -n -u $MAPR_CONTAINER_USER \
         -g ${MAPR_CONTAINER_GROUP:-$MAPR_GROUP} "$0" "$@"
-fi
+##fi
 
 
 
